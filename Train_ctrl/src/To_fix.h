29e7bdc3a6d719287172d2867ef29e5ce7c2e3a0
YigalB1 <byigal@gmail.com>
2024-09-02 21:37:50 +0300

    working

--- Train_ctrl/src/To_fix.h
+++ Train_ctrl/src/To_fix.h
@@ -5,6 +5,8 @@
 2- Switch debounce: 10K, 1K, 2,2uF ( or 1uF or 10uf)
 3- Fix PCB: Main leds were wrongly connected to input GPIOs
 4- Fix PCB: No more direct connect PCB to box cover. Too complicated and PCB size. Use wires.
+5- Fix PCB: remoce DC2dc from 24V to 5V, Use external 5V supply
+6- Fix PCB:Add Power switch for 5V input.
 
 
 
